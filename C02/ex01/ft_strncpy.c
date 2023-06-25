678a3b00d5726cdd28c187edfe21b3b0119bbb26
cheongnicole <132968349+cheongnicole@users.noreply.github.com>
2023-06-25 21:51:51 +0800

    Add files via upload

--- C02/ex01/ft_strncpy.c
+++ C02/ex01/ft_strncpy.c
@@ -6,30 +6,24 @@
 /*   By: ncheong <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/22 14:03:15 by ncheong           #+#    #+#             */
-/*   Updated: 2023/06/25 18:08:13 by ncheong          ###   ########.fr       */
+/*   Updated: 2023/06/25 21:42:24 by ncheong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int	is_shorter;
 	unsigned int	i;
 
 	i = 0;
-	is_shorter = 0;
-	while (i < n)
+	while (i < n && src[i] != '\0')
 	{
-		if (is_shorter || src[i] == '\0')
-		{
-			is_shorter = 1;
-			dest[i] = '\0';
-		}
-		else
-		{
-			dest[i] = src[i];
-		}
+		dest[i] = src[i];
 		i++;
 	}
-	dest[i] = '\0';
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
 	return (dest);
 }
