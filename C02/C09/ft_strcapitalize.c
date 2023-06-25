a4950f87a5b534027ad9882b189bea49b2746f71
cheongnicole <132968349+cheongnicole@users.noreply.github.com>
2023-06-25 20:54:42 +0800

    Add files via upload

---
+++ C02/C09/ft_strcapitalize.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncheong <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/25 20:06:57 by ncheong           #+#    #+#             */
+/*   Updated: 2023/06/25 20:49:37 by ncheong          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strlowcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] += 32;
+		i++;
+	}
+	return (str);
+}
+
+char	*ft_strcapitalize(char *str)
+{
+	int	i;
+	int	first;
+
+	i = 0;
+	first = 1;
+	ft_strlowcase(str);
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			if (first == 1)
+				str[i] -= 32;
+			first = 0;
+		}
+		else if (str[i] >= '0' && str[i] <= '9')
+			first = 0;
+		else
+			first = 1;
+		i++;
+	}
+	return (str);
+}
