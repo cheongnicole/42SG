86d4bdf3c60a729c991c2056682578b82fd54451
cheongnicole <132968349+cheongnicole@users.noreply.github.com>
2023-07-01 11:42:19 +0800

    changed int to long

--- C05/ex05/ft_sqrt.c
+++ C05/ex05/ft_sqrt.c
@@ -1,4 +1,30 @@
-int  ft_sqrt(int nb)
+int	ft_sqrt(int nb)
 {
-  return (0);
+	long	i;
+
+	if (nb <= 0 || nb == 2 || nb == 3 || nb == 5)
+		return (0);
+	if (nb == 1)
+		return (1);
+	if (nb <= 10000)
+		i = 2;
+	else if (nb <= 1000000)
+		i = 101;
+	else if (nb <= 100000000)
+		i = 1001;
+	else if (nb <= 400000000)
+		i = 10001;
+	else if (nb <= 900000000)
+		i = 20001;
+	else if (nb <= 1600000000)
+		i = 30001;
+	else
+		i = 40001;
+	while (i * i <= nb)
+	{
+		if (i * i == nb)
+			return i;
+		i++;
+	}
+	return (0);
 }
