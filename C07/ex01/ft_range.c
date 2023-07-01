e08a0cfe3855a80449870a8a859f1c3dc556c7ea
cheongnicole <132968349+cheongnicole@users.noreply.github.com>
2023-07-01 18:13:32 +0800

    create ft_range.c

--- C07/ex01/ft_range.c
+++ C07/ex01/ft_range.c
@@ -1,1 +1,22 @@
+#include <stddef.h>
+#include <stdlib.h>
 
+int	*ft_range(int min, int max)
+{
+	int	*dest;
+	int	i;
+
+	i = 0;
+	if (min >= max)
+		return (NULL);
+	dest = (int *)malloc((max - min) * sizeof(int));
+	if (dest)
+	{
+		while (i != (max - min))
+		{
+			dest[i] = min + i;
+			i++;
+		}
+	}
+	return (dest);
+}
