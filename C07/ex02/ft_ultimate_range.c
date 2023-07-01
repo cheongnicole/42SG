aade9e0a336c0272100ee4a449cba459b0eb2b3e
cheongnicole <132968349+cheongnicole@users.noreply.github.com>
2023-07-01 18:37:18 +0800

    Create ft_ultimate_range.c

--- C07/ex02/ft_ultimate_range.c
+++ C07/ex02/ft_ultimate_range.c
@@ -1,1 +1,26 @@
+#include <stddef.h>
+#include <stdlib.h>
 
+int	ft_ultimate_range(int **range, int min, int max)
+{
+	int	i;
+	int	*dest;
+
+	i = 0;
+	if (min >= max)
+	{
+		*range = NULL;
+		return (-1);
+	}
+	dest = (int *)malloc((max - min) * sizeof(int));
+	if (dest)
+	{
+		while (i != (max - min))
+		{
+			dest[i] = min + i;
+			i++;
+		}
+	}
+	*range = dest;
+	return (i);
+}
