04f617b7717794c9ec09e92cc095cd293bed6418
cheongnicole <132968349+cheongnicole@users.noreply.github.com>
2023-07-01 18:11:31 +0800

    add ft_strdup.c

--- C07/ex00/ft_strdup.c
+++ C07/ex00/ft_strdup.c
@@ -1,1 +1,30 @@
+#include <stdlib.h>
 
+int	ft_strlen(char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*dest;
+	int		i;
+
+	i = 0;
+	dest = (char *)malloc(ft_strlen(src) + 1);
+	if (dest)
+	{
+		while (src[i] != '\0')
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	return (dest);
+}
