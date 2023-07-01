cf2ba204e054cc83320cdcec7c7bc0e7b9e71087
cheongnicole <132968349+cheongnicole@users.noreply.github.com>
2023-07-01 22:19:08 +0800

    Create ft_boolean.h

--- C08/ex01/ft_boolean.h
+++ C08/ex01/ft_boolean.h
@@ -1,1 +1,17 @@
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
 
+# include <unistd.h>
+
+typedef enum t_bool
+{
+	FALSE = 0,
+	TRUE = 1,
+}	t_bool;
+
+# define EVEN(number) (number % 2 == 0)
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+# define SUCCESS 0
+
+#endif
