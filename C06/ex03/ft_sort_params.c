b24e14a51c0ec7a62c0dcc570cbce144cac0670a
cheongnicole <132968349+cheongnicole@users.noreply.github.com>
2023-07-03 10:25:52 +0800

    Add files via upload

---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncheong <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/29 12:20:11 by ncheong           #+#    #+#             */
+/*   Updated: 2023/06/29 14:28:27 by ncheong          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	current;
+
+	current = 0;
+	while (str[current] != '\0')
+	{
+		write(1, &str[current], 1);
+		current++;
+	}
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+	int	diff;
+
+	i = 0;
+	diff = 0;
+	while (s1[i] != '\0' || s2[i] != '\0')
+	{
+		diff = s1[i] - s2[i];
+		i++;
+		if (diff != 0)
+			return (diff);
+	}
+	return (diff);
+}
+
+void	swap_str(char **a, char **b)
+{
+	char	*tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (i < argc - 1)
+	{
+		j = i + 1;
+		while (j < argc)
+		{
+			if (ft_strcmp(argv[i], argv[j]) > 0)
+				swap_str(&argv[i], &argv[j]);
+			j++;
+		}
+		i++;
+	}
+	i = 1;
+	while (i < argc)
+	{
+		ft_putstr(argv[i]);
+		write(1, &"\n", 1);
+		i++;
+	}
+	return (0);
+}
