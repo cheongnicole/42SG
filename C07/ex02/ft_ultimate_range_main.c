621871b2e1fdded26e32dff94842241fb108f5ad
cheongnicole <132968349+cheongnicole@users.noreply.github.com>
2023-07-03 15:04:13 +0800

    Add files via upload

--- C07/ex02/ft_ultimate_range_main.c
+++ C07/ex02/ft_ultimate_range_main.c
@@ -1,38 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ultimate_range.c                                :+:      :+:    :+:   */
+/*   ft_ultimate_range_main.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ncheong <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/03 14:58:59 by ncheong           #+#    #+#             */
-/*   Updated: 2023/07/03 14:59:00 by ncheong          ###   ########.fr       */
+/*   Created: 2023/07/03 15:02:18 by ncheong           #+#    #+#             */
+/*   Updated: 2023/07/03 15:03:23 by ncheong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
 
-int	ft_ultimate_range(int **range, int min, int max)
-{
-	int	i;
-	int	*dest;
+int	ft_ultimate_range(int **range, int min, int max);
 
+int	main(void)
+{
+	int	min;
+	int	max;
+	int	*int_arr;
+	int	size;
+	int	i;
+
+	min = 3;
+	max = 7;
+	size = ft_ultimate_range(&int_arr, min, max);
 	i = 0;
-	if (min >= max)
+	while (i < size)
 	{
-		*range = NULL;
-		return (-1);
+		printf("%d ", int_arr[i]);
+		i++;
 	}
-	dest = (int *)malloc((max - min) * sizeof(int));
-	if (dest)
-	{
-		while (i != (max - min))
-		{
-			dest[i] = min + i;
-			i++;
-		}
-	}
-	*range = dest;
-	return (i);
+	free(int_arr);
+	return (0);
 }
