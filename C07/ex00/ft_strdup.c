ad27fb980deaf9843c99ebd505024f562fe9e1e3
cheongnicole <132968349+cheongnicole@users.noreply.github.com>
2023-07-03 14:56:47 +0800

    Add files via upload

--- C07/ex00/ft_strdup.c
+++ C07/ex00/ft_strdup.c
@@ -1,14 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncheong <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/29 17:47:57 by ncheong           #+#    #+#             */
+/*   Updated: 2023/06/29 19:00:56 by ncheong          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
-
-int	ft_strlen(char *src)
-{
-	int	i;
-
-	i = 0;
-	while (src[i] != '\0')
-		i++;
-	return (i);
-}
 
 char	*ft_strdup(char *src)
 {
@@ -16,15 +18,17 @@ char	*ft_strdup(char *src)
 	int		i;
 
 	i = 0;
-	dest = (char *)malloc(ft_strlen(src) + 1);
-	if (dest)
+	while (src[i])
+		i++;
+	dest = (char *)malloc(i);
+	if (!dest)
+		return (0);
+	i = 0;
+	while (src[i])
 	{
-		while (src[i] != '\0')
-		{
-			dest[i] = src[i];
-			i++;
-		}
-		dest[i] = '\0';
+		dest[i] = src[i];
+		i++;
 	}
+	dest[i] = '\0';
 	return (dest);
 }
