e3791f5f7799491af450a1923bd8d20ea99666d8
cheongnicole <132968349+cheongnicole@users.noreply.github.com>
2023-07-03 10:28:45 +0800

    Add files via upload

---
+++ C05/ex03/ft_recursive_power.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncheong <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/29 07:32:11 by ncheong           #+#    #+#             */
+/*   Updated: 2023/06/29 08:55:40 by ncheong          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_power(int nb, int power)
+{
+	if (power < 0)
+		return (0);
+	else if (power == 0)
+		return (1);
+	else
+		return (nb * ft_recursive_power(nb, power - 1));
+}
