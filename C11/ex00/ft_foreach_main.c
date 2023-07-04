598b02fac83466767865805f58eaaa131bbda674
cheongnicole <132968349+cheongnicole@users.noreply.github.com>
2023-07-04 16:23:04 +0800

    Add files via upload

--- C11/ex00/ft_foreach_main.c
+++ C11/ex00/ft_foreach_main.c
@@ -1,23 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_foreach.c                                       :+:      :+:    :+:   */
+/*   ft_foreach_main.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ncheong <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/04 15:09:08 by ncheong           #+#    #+#             */
-/*   Updated: 2023/07/04 16:11:37 by ncheong          ###   ########.fr       */
+/*   Created: 2023/07/04 15:11:30 by ncheong           #+#    #+#             */
+/*   Updated: 2023/07/04 16:12:53 by ncheong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_foreach(int *tab, int length, void(*f)(int))
+#include <stdio.h>
+
+void	ft_foreach(int *tab, int length, void(*f)(int));
+
+void	ft_putnbr(int nb);
+
+int	main(void)
 {
 	int	i;
+	int	table[10];
 
 	i = 0;
-	while (i < length)
+	while (i < 10)
 	{
-		f(tab[i]);
+		table[i] = i;
 		i++;
 	}
+	ft_foreach(table, 10, &ft_putnbr);
+	return (0);
 }
--- C11/ex00/ft_putnbr.c
+++ C11/ex00/ft_putnbr.c
@@ -1,23 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_foreach.c                                       :+:      :+:    :+:   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ncheong <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/04 15:09:08 by ncheong           #+#    #+#             */
-/*   Updated: 2023/07/04 16:11:37 by ncheong          ###   ########.fr       */
+/*   Created: 2023/06/27 22:07:47 by ncheong           #+#    #+#             */
+/*   Updated: 2023/06/29 06:37:19 by ncheong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_foreach(int *tab, int length, void(*f)(int))
-{
-	int	i;
+#include <unistd.h>
 
-	i = 0;
-	while (i < length)
+// need to take care of core dump in the largest negative integer
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
 	{
-		f(tab[i]);
-		i++;
+		write(1, "-2", 2);
+		ft_putnbr(147483648);
 	}
+	else if (nb < 0)
+	{
+		write(1, "-", 1);
+		ft_putnbr(nb * -1);
+	}
+	else if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+		write(1, &"0123456789"[nb], 1);
 }
