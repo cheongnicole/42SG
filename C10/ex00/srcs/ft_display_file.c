056d120c487a7d3a696fd9b4bd112d65c0bd0f0f
cheongnicole <132968349+cheongnicole@users.noreply.github.com>
2023-07-05 09:01:52 +0800

    Add files via upload

--- C10/ex00/srcs/ft_display_file.c
+++ C10/ex00/srcs/ft_display_file.c
@@ -1,1 +1,23 @@
+#include <unistd.h>
+#include <fcntl.h>
 
+int	main(int argc, char **argv)
+{
+	int		fd;
+	char	c;
+
+	if (argc == 1)
+		return (write(2, "File name missing.\n", 19));
+	else if (argc > 2)
+		return (write(2, "Too many arguments.\n", 20));
+	else
+	{
+		fd = open(argv[1], O_RDONLY);
+		if (fd < 0)
+			return (write(2, "Cannot read file.\n", 18));
+		while (read(fd, &c, 1))
+			write(1, &c, 1);
+	}
+	close(fd);
+	return (0);
+}
