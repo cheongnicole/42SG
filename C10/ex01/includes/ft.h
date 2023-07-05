2e0d49c78a992fb7ad0e3ea348529436995bf2b0
cheongnicole <132968349+cheongnicole@users.noreply.github.com>
2023-07-05 17:52:29 +0800

    Add files via upload

--- C10/ex01/includes/ft.h
+++ C10/ex01/includes/ft.h
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncheong <ncheong@student.42singapore.sg>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/05 16:38:26 by ncheong           #+#    #+#             */
+/*   Updated: 2023/07/05 16:39:37 by ncheong          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_H
 # define FT_H
 
-#include <unistd.h>
-#include <fcntl.h>
-#include <string.h>
-#include <errno.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <string.h>
+# include <errno.h>
 
 void	ft_putchar(char c);
 void	ft_swap(int *a, int *b);
