ca2843bb3f0ab5c0e9e9e6ba6a39ab5e71f2cff3
cheongnicole <132968349+cheongnicole@users.noreply.github.com>
2023-07-05 14:51:50 +0800

    Add files via upload

--- C10/ex02/includes/ft.h
+++ C10/ex02/includes/ft.h
@@ -1,1 +1,17 @@
+#ifndef FT_H
+# define FT_H
 
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+int		ft_atoi(char *str);
+
+#endif
