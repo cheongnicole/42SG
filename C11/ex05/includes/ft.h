37b8c4964c65eb6cf549bbcc04c6aa7c643fa029
cheongnicole <132968349+cheongnicole@users.noreply.github.com>
2023-07-06 06:58:24 +0800

    Add files via upload

--- C11/ex05/includes/ft.h
+++ C11/ex05/includes/ft.h
@@ -1,1 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncheong <ncheong@student.42singapore.sg>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/05 18:19:05 by ncheong           #+#    #+#             */
+/*   Updated: 2023/07/06 06:51:59 by ncheong          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#ifndef FT_H
+# define FT_H
+
+# include <unistd.h>
+
+int		ft_strcmp(char *s1, char *s2);
+void	ft_putstr(char *str);
+int		ft_atoi(char *str);
+void	ft_putlnbr(long lnbr);
+void	ft_putchar(char c);
+void	op_add(int val1, int val2);
+void	op_sub(int val1, int val2);
+void	op_mul(int val1, int val2);
+void	op_div(int val1, int val2);
+void	op_mod(int val1, int val2);
+
+#endif
