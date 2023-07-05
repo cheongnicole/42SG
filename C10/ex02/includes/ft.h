ed2314ca8c2d30bb8dca8702d6047523c48c5b6e
cheongnicole <132968349+cheongnicole@users.noreply.github.com>
2023-07-05 18:17:02 +0800

    Add files via upload

--- C10/ex02/includes/ft.h
+++ C10/ex02/includes/ft.h
@@ -1,11 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncheong <ncheong@student.42singapore.sg>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/05 17:54:14 by ncheong           #+#    #+#             */
+/*   Updated: 2023/07/05 18:08:15 by ncheong          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_H
 # define FT_H
 
-#include <unistd.h>
-#include <fcntl.h>
-#include <string.h>
-#include <errno.h>
-#include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <string.h>
+# include <errno.h>
+# include <stdlib.h>
 
 void	ft_putchar(char c);
 void	ft_swap(int *a, int *b);
@@ -13,5 +25,8 @@ void	ft_putstr(char *str);
 int		ft_strlen(char *str);
 int		ft_strcmp(char *s1, char *s2);
 int		ft_atoi(char *str);
+void	read_std_input(void);
+void	output_error(int err, char *filename);
+void	print_error(char *desc, char *err_arg);
 
 #endif
