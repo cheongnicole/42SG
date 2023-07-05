16112d192f1b8f655caf35e460b3ce2779ca34ff
cheongnicole <132968349+cheongnicole@users.noreply.github.com>
2023-07-05 18:17:52 +0800

    Add files via upload

---
+++ C10/ex02/srcs/ft_err_handle.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_err_handle.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncheong <ncheong@student.42singapore.sg>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/05 18:03:41 by ncheong           #+#    #+#             */
+/*   Updated: 2023/07/05 18:04:45 by ncheong          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft.h"
+
+void	read_std_input(void)
+{
+	char	c;
+
+	while (read(0, &c, 1))
+	{
+		if (c == '\n')
+			break ;
+	}
+}
+
+void	output_error(int err, char *filename)
+{
+	if (!err)
+		return ;
+	write(2, "tail: ", 5);
+	write(2, filename, ft_strlen(filename));
+	write(2, ": ", 2);
+	write(2, strerror(err), ft_strlen(strerror(err)));
+	write(2, "\n", 1);
+}
+
+void	print_error(char *err_desc, char *err_arg)
+{
+	write(2, "tail: ", 6);
+	write(2, err_desc, ft_strlen(err_desc));
+	write(2, ": ", 2);
+	write(2, err_arg, ft_strlen(err_arg));
+	write(2, "\n", 1);
+}
--- C10/ex02/srcs/ft_tail.c
+++ C10/ex02/srcs/ft_tail.c
@@ -1,56 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_tail.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncheong <ncheong@student.42singapore.sg>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/05 17:55:42 by ncheong           #+#    #+#             */
+/*   Updated: 2023/07/05 18:10:48 by ncheong          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft.h"
 
 // ft_tail mimics tail command
 // only to be invoked with -c option
 // With no FILE, or when FILE is -, read standard input
 
-void	read_std_input()
-{
-	char	c;
-
-	while (read(0, &c, 1))
-	{
-		if (c == '\n')
-			break;
-	}
-}
-
-void	output_error(int err, char *filename)
-{
-	if (!err)
-		return;
-	write(2, "tail: ", 5);
-	write(2, filename, ft_strlen(filename));
-	write(2, ": ", 2);
-	write(2, strerror(err), ft_strlen(strerror(err)));
-	write(2, "\n", 1);
-}
-
-void	print_error(char *err_desc, char* err_arg)
-{
-	write(2, "tail: ", 6);
-	write(2, err_desc, ft_strlen(err_desc));
-	write(2, ": ", 2);
-	write(2, err_arg, ft_strlen(err_arg));
-	write(2, "\n", 1);
-}
-
 // returns number of bytes to read if valid
 int	check_args(int argc, char **argv)
 {
 	int	bytes;
-	
+
 	if (argc < 4)
 	{
 		print_error("invalid number of arguments", "");
 		return (0);
 	}
-	if (ft_strcmp(argv[1] , "-c") != 0)
+	if (ft_strcmp(argv[1], "-c") != 0)
 	{
 		print_error("invalid option", argv[1]);
 		return (0);
 	}
-	if ((bytes = ft_atoi(argv[2])) <= 0)
+	bytes = ft_atoi(argv[2]);
+	if (bytes <= 0)
 	{
 		print_error("invalid number of bytes", argv[2]);
 		return (0);
@@ -70,10 +52,10 @@ void	print_header(char *header)
 
 int	file_size(char *filename)
 {
-	int	i;
-	int	fd;
+	int		i;
+	int		fd;
 	char	c;
-	
+
 	fd = open(filename, O_RDONLY);
 	i = 0;
 	while (read(fd, &c, 1))
@@ -84,9 +66,9 @@ int	file_size(char *filename)
 
 void	print_tail(int bytes, char *filename)
 {
-	int	fd;
-	int	start;
-	int	end;
+	int		fd;
+	int		start;
+	int		end;
 	char	*buffer;
 
 	while (ft_strcmp(filename, "-") == 0)
@@ -114,8 +96,9 @@ int	main(int argc, char **argv)
 	int	i;
 	int	bytes;
 
-	if (!(bytes = check_args(argc, argv)))
-		return 0;
+	bytes = check_args(argc, argv);
+	if (!(bytes))
+		return (0);
 	i = 3;
 	while (i < argc)
 	{
