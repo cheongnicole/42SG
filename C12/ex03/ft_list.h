c66de7333f00352b6b2930a0f1a9760a5a3bacf1
cheongnicole <132968349+cheongnicole@users.noreply.github.com>
2023-07-11 14:52:58 +0800

    Add files via upload

--- C12/ex03/ft_list.h
+++ C12/ex03/ft_list.h
@@ -6,7 +6,7 @@
 /*   By: ncheong <ncheong@student.42singapore.sg>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/08 23:01:05 by ncheong           #+#    #+#             */
-/*   Updated: 2023/07/09 00:52:41 by ncheong          ###   ########.fr       */
+/*   Updated: 2023/07/11 14:43:05 by ncheong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,8 @@ typedef struct s_list
 }					t_list;
 
 t_list	*ft_create_elem(void *data);
+void	ft_list_push_front(t_list **begin_list, void *data);
+int	ft_list_size(t_list *begin_list);
+t_list	*ft_list_last(t_list *begin_list);
 
 #endif
---
+++ C12/ex03/ft_list_last_main.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_last_main.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncheong <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/11 14:45:03 by ncheong           #+#    #+#             */
+/*   Updated: 2023/07/11 14:50:58 by ncheong          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "ft_list.h"
+
+int	main(void)
+{
+	t_list	*begin_list;
+	t_list	*list;
+	char	*str1 = "first";
+	char	*str2 = "second";
+	char	*str3 = "third";
+	
+	// ex00 ft_create_elem
+	begin_list = ft_create_elem(str1);
+
+	// ex01 ft_list_push_front
+	ft_list_push_front(&begin_list, str2);
+	ft_list_push_front(&begin_list, str3);
+	list = begin_list;
+	while (list->next != NULL)
+	{
+		printf("%s\n", (char *)list->data);
+		list = list->next;
+	}
+	printf("%s\n", (char *)list->data);
+
+	// ex02 ft_list_size
+	printf("%d elements\n", ft_list_size(begin_list));
+
+	// ex03 ft_list_last
+	list = ft_list_last(begin_list);
+	printf("%s\n", (char *)list->data);
+	return (0);
+}
